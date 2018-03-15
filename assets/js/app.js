7a032abe0820fc1dd63071ada673551559b31fe9
Scott Barona <sbrona317@gmail.com>
2018-03-14 21:04:38 -0400

    js files

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+// Groupon API | (token auth)
+// -----------
+
+// Google API | api key: AIzaSyDRsx9FnAROae4cLDp1FiJhKYChqD7Bejg
+// -----------
+
+// Weather API | api key: 8fa5bed612da59c7d341e2eeefbe2d3f
+// -----------
+
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB1NNGKvL9KRoL3siEyKCMA0_lKV5xU5Xc",
+    authDomain: "dosomethingdb.firebaseapp.com",
+    databaseURL: "https://dosomethingdb.firebaseio.com",
+    projectId: "dosomethingdb",
+    storageBucket: "dosomethingdb.appspot.com",
+    messagingSenderId: "473809310213"
+  };
+  firebase.initializeApp(config);
+
+  var db = firebase.database();
+
