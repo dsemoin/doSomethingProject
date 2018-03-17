7586fe99f5bd57ddfce35162e3fe4cbd0863d149
Josh <josh@Joshs-MacBook-Pro.local>
2018-03-17 14:02:35 -0400

    map and card viewer

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,14 +21,16 @@
 
   var db = firebase.database();
 
-  weather();
-  google();
+  // weather();
+  // google();
 
   // Dynamically Generate divs
 
   var cardList = ['Eat', 'Entertain', 'Learn', 'Exercise']
 
   var cuisineList = ['American', 'Chinese', 'Mexican', 'Italian',]
+
+  var inOrOut  = []
 
   function generateCards () {
     for (var i = 0; i < cardList.length; i++) {
@@ -52,9 +54,50 @@ $('.card').click(function () {
     cardList = cuisineList;
     generateCards(); 
   }
+});
 
-
+$('#start').click(function() {
+  $('.card-viewer').removeClass('hidden');
 })
+
+var map, infoWindow;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 6
+        });
+        infoWindow = new google.maps.InfoWindow;
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
+
+initMap();
 
 
 
